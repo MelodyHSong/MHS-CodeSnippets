9ef22607dd03e11df93168bd6a4695a080358f4b
☆ Melody H. Song ☆ <55931729+MelodyHSong@users.noreply.github.com>
2025-09-24 21:43:57 -0400

    Add template script for Unity MonoBehaviour

---
+++ CSharp/Scripts/_monoTEMPLATE.cs
@@ -0,0 +1,48 @@
+/*
+✰
+✰ Author: ✰ [Your Name Here] ✰
+✰ Language: C#
+✰ File Name: [New File Name]
+✰ Date: [Current Date]
+✰
+*/
+
+using UnityEngine;
+
+// Description: 
+
+public class _monoTEMPLATE : MonoBehaviour
+{
+    // UwU - Put your variables here! ✰
+
+
+    void Awake()
+    {
+        // owo Use Awake() to initialize! ✰
+    }
+    
+    void Start()
+    {
+        // owo Use Start() for initialization before the first frame! ✰
+    }
+
+    void Update()
+    {
+        // owo Update is called once per frame! ✰
+    }
+
+    void FixedUpdate()
+    {
+        // owo FixedUpdate is for physics calculations! ✰
+    }
+}
+
+/*
+✰
+✰ MonoBehaviour   : The base class for every script that is attached to a Unity object. ✰
+✰ Awake()         : Called when the script instance is being loaded, before Start(). ✰
+✰ Start()         : Called on the frame when a script is enabled, just before any of the Update methods are called for the first time. ✰
+✰ Update()        : The most common update function, called once per frame. ✰
+✰ FixedUpdate()   : Called at a fixed frame rate, often used for physics updates. ✰
+✰
+*/
